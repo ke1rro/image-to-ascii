4ed383cf4cd9d23f978062a79608a9a97902ca7f
ke1rro <nikitalenyk2@gmail.com>
2024-05-12 20:11:34 +0300

    fix

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import PIL.Image
 from file_format import get_file_format
+
 ASCII_CHARS = ['@', '#', '$',
                '&', '?', '{',
                '}', '[', ']'
                '(', ')', '\\',
                '/',  '*', '=',
                '-' '+', ';',
-               ':', ',', '.']
-NEW_WIDTH = 300
+               ':', ',', '.'
+               ]
+MAX_WIDTH = 300
 
 
 def image_grayscale(image):
@@ -15,9 +17,9 @@ def image_grayscale(image):
     return image
 
 
-def resize(image, new_width=NEW_WIDTH):
+def resize(image, new_width=MAX_WIDTH):
     current_width, current_heigth = image.size
-    ratio = current_heigth / current_width
+    ratio = current_heigth / current_width / 1.6
     new_heigth = int(new_width * ratio)
     resized_image = image.resize((new_width, new_heigth))
     return resized_image
@@ -25,24 +27,23 @@ def resize(image, new_width=NEW_WIDTH):
 
 def get_pixel(image):
     pixels = image.getdata()
-    pixeld_image = "".join([ASCII_CHARS[pixel // 30] for pixel in pixels])
+    pixeld_image = "".join([ASCII_CHARS[pixel // 49] for pixel in pixels])
     return pixeld_image
 
 
-def get_image(new_width=NEW_WIDTH):
+def get_image(new_width=MAX_WIDTH):
     while True:
         try:
             file_format = get_file_format()
-            file_name = input('Please enter the file name located on Desktop of your PC: ')
-            path = f'D:\\Desktop\\{file_name}.{file_format}'
-            print(f'Your file is {path}')
+            file_name = input('Please enter the file name located in the same folder as the python file: ')
+            path = f'{file_name}.{file_format}'
             image_to_convert = PIL.Image.open(path)
             new_image = get_pixel(image_grayscale(resize(image_to_convert)))
             pixel_count = len(new_image)
             ascii_image = '\n'.join(new_image[i:(i+new_width)] for i in range(0, pixel_count, new_width))
             print(ascii_image)
 
-            with open(file='D:\\Desktop\\ASCII_img.txt', mode='w') as file:
+            with open(file='ASCII_img.txt', mode='w') as file:
                 for line in ascii_image:
                     file.write(line)
 
